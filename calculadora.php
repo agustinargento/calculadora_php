d4d6eb16c7e20c0dadebadce37893164b7d630d1
agvs <a_d_argento@hotmail.com>
2022-09-16 10:48:18 -0300

    Calculadora solo suma

---
+++ calculadora.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    <form class="caja" action="calculadora.php" method="get">
+    <h1>CALCULADORA PHP</h1>
+        <input type="text" name="numero1" value="" placeholder="ingrese un número">
+        <input type="text" name="numero2" value="" placeholder="ingrese un número">
+        <input type="submit" name="" value="Respuesta">
+        <output type = "number" name="">
+    </form>
+</body>
+</html>
+<?php 
+
+if(isset($_GET['numero1']) && isset($_GET['numero2']) && is_numeric($_GET['numero1']) 
+&& is_numeric($_GET['numero2']));
+{
+    $numero1 = $_GET['numero1'];
+    $numero2 = $_GET['numero2'];
+    echo $numero1 + $numero2;
+
+}
+
+?>
